c10e1facd5e6852fe0e55299306ffe53caefd96c
Rfluid <ruy.vieiraneto@gmail.com>
2025-11-13 00:49:11 -0300

    Prepare lite version: remove premium content and regenerate docs

--- src/campaign/service/synchronize-contact.go
+++
@@ -1,7 +0,0 @@
-package campaign_service
-
-import synch_service "github.com/Astervia/wacraft-core/src/synch/service"
-
-func CreateContactSynchronizer() *synch_service.MutexSwapper[string] {
-	return synch_service.CreateMutexSwapper[string]()
-}
